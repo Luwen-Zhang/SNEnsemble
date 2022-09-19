905cdc8174894b7b4b0a88a9abe1db8e505dadd6
LuoXueling <sharrin@sjtu.edu.cn>
2022-09-19 22:53:43 +0800

    Format

--- src/extractor.py
+++ src/extractor.py
@@ -2,12 +2,21 @@ from random import Random
 import pandas as pd
 import numpy as np
 
+
 class Extractor():
     def __init__(self, data_path):
+        '''
+        The automatic article data extractor class.
+        :param data_path: The path of the formatted .xlsx file.
+        '''
         self.data_path = data_path
 
     def read(self):
-        self.df = pd.read_excel(self.data_path, engine = 'openpyxl', sheet_name=None)
+        '''
+        Read the formatted .xlsx file from data_path.
+        :return: None
+        '''
+        self.df = pd.read_excel(self.data_path, engine='openpyxl', sheet_name=None)
 
         # Read sheets and classify
         self.sheet_names = list(self.df.keys())
@@ -33,11 +42,11 @@ class Extractor():
         self.feature_names = self.scatter_feature_names.copy()
         for property in self.properties:
             self.feature_names += list(self.df[property].columns)[1:]
-        self.feature_names = list(sorted(set(self.feature_names),key=self.feature_names.index))
+        self.feature_names = list(sorted(set(self.feature_names), key=self.feature_names.index))
 
         assign = self.df['Assignment']
 
-        data = pd.DataFrame(columns=list(assign.columns)+self.feature_names)
+        data = pd.DataFrame(columns=list(assign.columns) + self.feature_names)
 
         # For each scatter to be assigned, create a dataframe tmp_df that contains all feature values
         for row in assign.iterrows():
@@ -52,25 +61,25 @@ class Extractor():
                 assigned_features += assigned_property_feature
 
             # Create the dataframe for the scatter
-            tmp_df = pd.DataFrame(columns=list(row[1].keys())+list(self.df[scatter_name].columns)+assigned_features)
+            tmp_df = pd.DataFrame(columns=list(row[1].keys()) + list(self.df[scatter_name].columns) + assigned_features)
 
             # Assign scatter values
             scatter_df = self.df[scatter_name]
             for col_name in scatter_df.columns:
-                tmp_df[col_name]=scatter_df[col_name]
+                tmp_df[col_name] = scatter_df[col_name]
 
             # For each property, extract values and assign them to all scatter points
             for property in prop_to_assign:
                 assigned_property_feature = list(self.df[property].columns)[1:]
                 assigned_property_index = list(self.df[property]['Property']).index(row[1][property])
-                assigned_property_value = self.df[property].loc[assigned_property_index,assigned_property_feature]
+                assigned_property_value = self.df[property].loc[assigned_property_index, assigned_property_feature]
 
                 for row_idx in tmp_df.index:
-                    tmp_df.loc[row_idx,assigned_property_feature] = assigned_property_value
+                    tmp_df.loc[row_idx, assigned_property_feature] = assigned_property_value
 
             # Finally, assign information to the dataframe
             for row_idx in tmp_df.index:
-                tmp_df.loc[row_idx,row[1].keys()] = row[1].values
+                tmp_df.loc[row_idx, row[1].keys()] = row[1].values
 
             data = pd.concat([data, tmp_df], axis=0, ignore_index=True)
 
@@ -87,7 +96,14 @@ class Extractor():
         self.initial_data = data.copy()
 
     def apply_transform(self, column_from, column_to, transform_func, **kargs):
-
+        '''
+        Transform one feature to another according to a function. Only NaN values in column_to will be filled.
+        :param column_from: The input feature of transform_func
+        :param column_to: The output feature of transform_func
+        :param transform_func: User defined transformation function handler.
+        :param kargs: Parameters for transform_func
+        :return: A boolean value representing whether any value has been transformed.
+        '''
         # transform_idx represents indexes of feature values to be transformed in column_to
         # new_feature is a boolean represents whether a new feature would be created.
         transform_idx, new_feature = self.check_available_transform(column_from, column_to)
@@ -103,7 +119,12 @@ class Extractor():
             return True
 
     def check_available_transform(self, column_from, column_to):
-
+        '''
+        Check non-NaN values in features encountered in apply_transform()
+        :param column_from: The input feature of transform_func of apply_transform()
+        :param column_to: The output feature of transform_func of apply_transform()
+        :return: A list (ndarray) of transformable indexes; A boolean value representing whether a new feature will be created.
+        '''
         # column_from must exist in feature_names
         if column_from not in self.feature_names:
             return [], False
@@ -121,6 +142,13 @@ class Extractor():
             return transform_idx, False
 
     def fill_na(self, column, criterion, remove_na_axis):
+        '''
+        Fill NaN values of one feature using a machine learning model, taking other features as predictors.
+        :param column: The feature to be filled.
+        :param criterion: Machine learning model for NaN value prediction.
+        :param remove_na_axis: Indicates deleting rows(0) or columns(1) when other features contain NaN values.
+        :return: False if nothing has been filled, otherwise None.
+        '''
         if column not in self.feature_names:
             print('Feature does not exist.')
             return False
@@ -131,33 +159,34 @@ class Extractor():
         train_features.remove(column)
 
         data = self.data.copy()[train_features]
-        
+
         # Some features may contain NaN values, which should be dropped
         data = data.dropna(axis=remove_na_axis)
         if remove_na_axis == 1:
             # Reset train_features since some columns might be dropped
             train_features = list(data.columns)
 
-        column_data = self.data.copy().loc[data.index,column]
+        column_data = self.data.copy().loc[data.index, column]
         train_index = np.where(pd.notna(column_data))[0]
-        pred_index = np.setdiff1d(data.index,train_index)
+        pred_index = np.setdiff1d(data.index, train_index)
 
-        if len(pred_index)==0:
+        if len(pred_index) == 0:
             print('\tNo NaN to be filled.')
             return False
 
-        train_data = np.array(data.loc[train_index,train_features],dtype=np.float32)
-        train_label = np.array(column_data[train_index],dtype=np.float32)
-        pred_data = np.array(data.loc[pred_index,train_features],dtype=np.float32)
+        train_data = np.array(data.loc[train_index, train_features], dtype=np.float32)
+        train_label = np.array(column_data[train_index], dtype=np.float32)
+        pred_data = np.array(data.loc[pred_index, train_features], dtype=np.float32)
 
         if criterion == 'RandomForest':
             from sklearn.ensemble import RandomForestRegressor
 
             rf = RandomForestRegressor(n_jobs=-1)
 
-            rf.fit(train_data,train_label)
-            print('\tPredictors: ',train_features)
-            print(f'\t{train_data.shape[0]} data as training set, {pred_data.shape[0]} to be predict, R2 score {rf.score(train_data, train_label)}.')
+            rf.fit(train_data, train_label)
+            print('\tPredictors: ', train_features)
+            print(
+                f'\t{train_data.shape[0]} data as training set, {pred_data.shape[0]} to be predict, R2 score {rf.score(train_data, train_label)}.')
 
             pred_label = rf.predict(pred_data)
 
@@ -169,38 +198,48 @@ class Extractor():
             return False
 
     def interpolate_from_curves(self):
-        
+        '''
+        Automatically interpolate missing values using Curve(s).
+        :return: None
+        '''
+
         def check_available_interp():
             # Return a ndarray that represents available transform numbers of curve relations and directions (x->y or y->x)
-            available_interp = np.zeros((len(self.curve_relation),2))
+            available_interp = np.zeros((len(self.curve_relation), 2))
             for idx, relation in enumerate(self.curve_relation):
-                available_interp[idx,0] = len(self.check_available_transform(relation[0], relation[1])[0])
-                available_interp[idx,1] = len(self.check_available_transform(relation[1], relation[0])[0])
+                available_interp[idx, 0] = len(self.check_available_transform(relation[0], relation[1])[0])
+                available_interp[idx, 1] = len(self.check_available_transform(relation[1], relation[0])[0])
             return available_interp
 
         available_interp = check_available_interp()
         while np.sum(available_interp) != 0:
             # Until all available interpolations are eliminated.
-            best_interp = np.where(available_interp==np.max(available_interp))
+            best_interp = np.where(available_interp == np.max(available_interp))
             best_interp_relation = best_interp[0][0]
             best_interp_from = best_interp[1][0]
-            
+
             self.interpolate_from_curve(self.curves[best_interp_relation], best_interp_from)
-            
+
             available_interp = check_available_interp()
 
     def interpolate_from_curve(self, curve_name, direction):
+        '''
+        Interpolate using specific Curve data.
+        :param curve_name: The name of Curve sheet.
+        :param direction: 0 to interpolate y from x. 1 to interpolate x from y.
+        :return: None
+        '''
         curve_data = self.df[curve_name]
         column_from = curve_data.columns[direction]
-        column_to = curve_data.columns[1-direction]
+        column_to = curve_data.columns[1 - direction]
         curve_x = curve_data[column_from].values
         curve_y = curve_data[column_to].values
 
         def interpolation(x):
             # This is a example of linear interpolation.
-            if x<np.min(curve_x) or x>np.max(curve_x):
+            if x < np.min(curve_x) or x > np.max(curve_x):
                 print('Interpolated data exceeds the curve range.')
-            return np.interp(x,curve_x,curve_y)
+            return np.interp(x, curve_x, curve_y)
 
         status = self.apply_transform(column_from, column_to, interpolation)
         if status:
@@ -216,5 +255,3 @@ class Extractor():
 
     def restore_data(self):
         self.data = self.initial_data.copy()
-            
-
