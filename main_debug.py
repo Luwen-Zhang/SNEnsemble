ee3dab6a53576be4fe04112b2c420e9f7e6ed8e5
LuoXueling <sharrin@sjtu.edu.cn>
2022-11-23 15:31:30 +0800

    Update several temp files.

--- main_debug.py
+++ main_debug.py
@@ -4,20 +4,20 @@ from src.core.model import *
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("Using {} device".format(device))
 
-configfile = 'base_Upwind_avg_fatigue'
+configfile = 'base_OptiMat_avg_fatigue'
 trainer = Trainer(device=device)
 trainer.load_config(configfile)
-# trainer.load_data()
-# trainer.describe()
+trainer.load_data()
 
-# models = [
-#     # AutoGluon(trainer),
-#     PytorchTabular(trainer),
-#     # TabNet(trainer),
-#     ThisWork(trainer)
-# ]
+trainer.describe()
 
-# trainer.add_modelbases(models)
+models = [
+    # AutoGluon(trainer),
+    # PytorchTabular(trainer),
+    ThisWork(trainer)
+]
 
-# trainer.train(verbose=True)
-# trainer.get_leaderboard(test_data_only=False)
+trainer.add_modelbases(models)
+
+trainer.train(verbose=True)
+trainer.get_leaderboard(test_data_only=False)
