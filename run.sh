986b3dc87a266b53efd9c58da7bae07b074cd6f3
LuoXueling <sharrin@sjtu.edu.cn>
2022-12-14 15:32:07 +0800

    Update run scripts.

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
-python main.py --base base_SNL_MSU_DOE_avg_static --bayes_opt
-python main.py --base base_SNL_MSU_DOE_avg_fatigue --bayes_opt
-python main.py --base base_OptiMat_avg_static --bayes_opt
-python main.py --base base_OptiMat_avg_fatigue --bayes_opt
-python main.py --base base_Upwind_avg_static --bayes_opt
-python main.py --base base_Upwind_avg_fatigue --bayes_opt
-python main.py --base base_FACT_avg_static --bayes_opt
-python main.py --base base_FACT_avg_fatigue --bayes_opt
+python main.py --base base_SNL_MSU_DOE_fatigue --bayes_opt
+python main.py --base base_OptiMat_fatigue --bayes_opt
+python main.py --base base_Upwind_fatigue --bayes_opt
+python main.py --base base_FACT_fatigue --bayes_opt
