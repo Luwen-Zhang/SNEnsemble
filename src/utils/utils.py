c5e46fbb27d9a39ef39153ddaaad0657abb666b1
LuoXueling <sharrin@sjtu.edu.cn>
2022-12-16 11:05:09 +0800

    Ignore DeprecationWarning because of np.int used in skopt.

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,6 +6,7 @@ import sys
 import warnings
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
+warnings.simplefilter(action="ignore", category=DeprecationWarning)
 
 import numpy as np
 import pandas as pd
