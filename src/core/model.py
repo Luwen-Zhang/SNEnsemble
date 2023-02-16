8044e010213bbd49a163744039c211c628ceb76e
LuoXueling <sharrin@sjtu.edu.cn>
2023-02-16 20:16:46 +0800

    Fix a bug in the usage of dataimputer

--- src/core/model.py
+++ src/core/model.py
@@ -1086,7 +1086,7 @@ class TorchModel(AbstractModel):
     def _predict(
         self, input_df: pd.DataFrame, model_name, additional_data: list = None, **kwargs
     ):
-        df = self.trainer.dataimputer.fit_transform(input_df.copy(), self.trainer)
+        df = self.trainer.dataimputer.transform(input_df.copy(), self.trainer)
         df = self.trainer._data_transform(df)
         X = torch.tensor(
             df[self.trainer.feature_names].values.astype(np.float32),
