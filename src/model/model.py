12d079370a9c7cb0a172cc721cf44951c435dff8
LuoXueling <sharrin@sjtu.edu.cn>
2023-03-03 00:12:01 +0800

    RFE works on TorchModel with multiple models.

--- src/model/model.py
+++ src/model/model.py
@@ -1547,7 +1547,7 @@ class RFE(TorchModel):
     def __init__(
         self,
         trainer: Trainer,
-        torch_model: Type[TorchModel],
+        modelbase: AbstractModel,
         model_subset=None,
         program=None,
         metric: str = "Validation RMSE",
@@ -1560,35 +1560,49 @@ class RFE(TorchModel):
         self.cross_validation = cross_validation
         self.min_features = min_features
 
-        self.internal_trainer = cp(trainer)
-        self.torch_model = torch_model
+        internal_trainer = cp(trainer)
+        internal_trainer.modelbases = []
+        internal_trainer.modelbases_names = []
+        self._model_names = modelbase.get_model_names()
+        self.model_class = modelbase.__class__
         super(RFE, self).__init__(
             trainer=trainer, program=program, model_subset=model_subset
         )
-        self.internal_trainer.project_root = self.root
-        self.modelbase = self.torch_model(self.internal_trainer)
-        self.internal_trainer.modelbases = []
-        self.internal_trainer.modelbases_names = []
-        self.internal_trainer.add_modelbases([self.modelbase])
-        self.metrics = []
-        self.features_eliminated = []
-        self.selected_features = []
-        self.impor_dicts = []
+        self.trainer_modelbase = {}
+
+        internal_trainer.project_root = self.root
+
+        for model_name in self.get_model_names():
+            tmp_trainer = cp(internal_trainer)
+            modelbase = self.model_class(tmp_trainer, model_subset=[model_name])
+            tmp_trainer.add_modelbases([modelbase])
+            self.trainer_modelbase[model_name] = (tmp_trainer, modelbase)
+        self.metrics = {}
+        self.features_eliminated = {}
+        self.selected_features = {}
+        self.impor_dicts = {}
 
     def _get_program_name(self):
-        return "RFE-" + self.torch_model.__name__
+        return "RFE-" + self.model_class.__name__
 
     def _get_model_names(self):
-        return self.modelbase.get_model_names()
+        return self._model_names
 
     def _new_model(self, model_name, verbose, **kwargs):
-        return self.modelbase._new_model(model_name, verbose, **kwargs)
+        return self.trainer_modelbase[model_name][1]._new_model(
+            model_name, verbose, **kwargs
+        )
 
     def _predict(self, df: pd.DataFrame, model_name, derived_data=None, **kwargs):
-        return self.modelbase._predict(df, model_name, derived_data, **kwargs)
+        return self.trainer_modelbase[model_name][1]._predict(
+            df, model_name, derived_data, **kwargs
+        )
 
     def _predict_all(self, **kwargs):
-        return self.modelbase._predict_all(**kwargs)
+        predictions = {}
+        for name, (trainer, modelbase) in self.trainer_modelbase.items():
+            predictions[name] = modelbase._predict_all(**kwargs)[name]
+        return predictions
 
     def _train(
         self,
@@ -1597,42 +1611,49 @@ class RFE(TorchModel):
         warm_start=False,
         **kwargs,
     ):
-        if warm_start:
-            self.modelbase._train(
-                warm_start=warm_start,
-                model_subset=model_subset,
-                verbose=verbose,
-                **kwargs,
-            )
-        else:
-            self.run(verbose=verbose, model_subset=model_subset)
-            self.modelbase._train(
-                warm_start=warm_start,
-                model_subset=model_subset,
-                verbose=verbose,
-                **kwargs,
-            )
+        for model_name in (
+            self.get_model_names() if model_subset is None else model_subset
+        ):
+            if warm_start:
+                self.trainer_modelbase[model_name][1]._train(
+                    warm_start=warm_start,
+                    model_subset=[model_name],
+                    verbose=verbose,
+                    **kwargs,
+                )
+            else:
+                self.run(verbose=verbose, model_name=model_name)
+                self.trainer_modelbase[model_name][1]._train(
+                    warm_start=warm_start,
+                    model_subset=[model_name],
+                    verbose=verbose,
+                    **kwargs,
+                )
 
-    def run(self, verbose=True, model_subset=None):
+    def run(self, model_name, verbose=True):
         rest_features = cp(self.trainer.all_feature_names)
+        trainer, modelbase = self.trainer_modelbase[model_name]
+        metrics = []
+        features_eliminated = []
+        impor_dicts = []
         while len(rest_features) > self.min_features:
             if verbose:
                 print(f"Using features: {rest_features}")
-            self.internal_trainer.set_feature_names(rest_features)
+            trainer.set_feature_names(rest_features)
             if self.cross_validation == 0:
-                self.modelbase._train(
-                    verbose=False, model_subset=model_subset, dump_trainer=False
+                modelbase._train(
+                    verbose=False, model_subset=[model_name], dump_trainer=False
                 )
-            leaderboard = self.internal_trainer.get_leaderboard(
+            leaderboard = trainer.get_leaderboard(
                 test_data_only=False,
                 cross_validation=self.cross_validation,
                 verbose=False,
                 dump_trainer=False,
             )
-            self.metrics.append(leaderboard.loc[0, self.metric])
-            importance, names = self.internal_trainer.cal_feature_importance(
-                program=self.modelbase.program,
-                model_name=self.modelbase.get_model_names()[0],
+            metrics.append(leaderboard.loc[0, self.metric])
+            importance, names = trainer.cal_feature_importance(
+                program=modelbase.program,
+                model_name=model_name,
                 method=self.impor_method,
             )
             impor_dict = {"feature": [], "attr": []}
@@ -1645,18 +1666,22 @@ class RFE(TorchModel):
             df.reset_index(drop=True, inplace=True)
             rest_features = list(df["feature"])
             print(rest_features)
-            self.features_eliminated.append(rest_features.pop(-1))
-            self.impor_dicts.append(df)
+            features_eliminated.append(rest_features.pop(-1))
+            impor_dicts.append(df)
             if verbose:
-                print(f"Eliminated feature: {self.features_eliminated[-1]}")
+                print(f"Eliminated feature: {features_eliminated[-1]}")
                 # print(f"Permutation importance:\n{df}")
 
-        select_idx = self.metrics.index(np.min(self.metrics))
-        self.selected_features = self.features_eliminated[select_idx:]
-        self.internal_trainer.set_feature_names(self.selected_features)
+        select_idx = metrics.index(np.min(metrics))
+        selected_features = features_eliminated[select_idx:]
+        trainer.set_feature_names(selected_features)
+        self.metrics[model_name] = metrics
+        self.features_eliminated[model_name] = features_eliminated
+        self.impor_dicts[model_name] = impor_dicts
+        self.selected_features[model_name] = selected_features
         if verbose:
-            print(f"Selected features: {self.selected_features}")
-            print(f"Eliminated features: {self.features_eliminated[:select_idx]}")
+            print(f"Selected features: {selected_features}")
+            print(f"Eliminated features: {features_eliminated[:select_idx]}")
 
 
 class ModelAssembly(AbstractModel):
