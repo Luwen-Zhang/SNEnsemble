6d9b8f9601264facd2fefb80076e197119b70c9a
LuoXueling <sharrin@sjtu.edu.cn>
2023-03-21 21:17:34 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-python main.py --base composite --bayes_opt
+python main.py --base composite --bayes_opt --data_splitter MaterialCycleSplitter
+python main.py --base composite --bayes_opt --data_splitter MaterialSplitter
+python main.py --base composite --bayes_opt --data_splitter CycleSplitter
+python main.py --base composite --bayes_opt --data_splitter RandomSplitter
 
