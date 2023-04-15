2b163f21fd9d6f4708ed675a4362eba2e2d9816d
LuoXueling <sharrin@sjtu.edu.cn>
2023-04-15 19:49:04 +0800

    Implement StressGradLoss.

--- src/model/_transformer/loss.py
+++ src/model/_transformer/loss.py
@@ -9,6 +9,28 @@ def BiasLoss(base_loss: torch.Tensor, w: torch.Tensor):
     return (base_loss * w).mean()
 
 
+def StressGradLoss(y_pred, s, base_loss, *data):
+    with torch_with_grad():
+        grad_s = torch.autograd.grad(
+            outputs=y_pred,
+            inputs=s,
+            grad_outputs=torch.ones_like(y_pred),
+            retain_graph=True,
+            create_graph=True,  # True to compute higher order derivatives, and is more expensive.
+        )[0].view(-1, 1)
+        grad_s_2 = torch.autograd.grad(
+            outputs=grad_s,
+            inputs=s,
+            grad_outputs=torch.ones_like(grad_s),
+            retain_graph=True,
+            create_graph=False,  # True to compute higher order derivatives, and is more expensive.
+        )[0].view(-1, 1)
+    grad_loss = torch.mean(F.relu(grad_s) ** 2)
+    grad_2_loss = torch.mean(F.relu(-grad_s_2) ** 2)
+    base_loss = base_loss + grad_loss + 1e3 * grad_2_loss
+    return base_loss
+
+
 def ConsGradLoss(balance=1e3, *data, **kwargs):
     with torch_with_grad():
         base_loss: torch.Tensor = kwargs["base_loss"]
