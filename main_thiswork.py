39e9f237130afe74cde3eb20e49cf6d71a248c40
LuoXueling <sharrin@sjtu.edu.cn>
2023-04-16 12:57:22 +0800

    Create main_thiswork.py

---
+++ main_thiswork.py
@@ -0,0 +1,26 @@
+import torch
+from src.trainer import Trainer
+from src.model import *
+from src.utils import Logging
+import os
+
+log = Logging()
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+
+trainer = Trainer(device=device)
+trainer.load_config()
+log.enter(os.path.join(trainer.project_root, "log.txt"))
+trainer.summarize_setting()
+trainer.load_data()
+models = [
+    Transformer(
+        trainer,
+        model_subset=[
+            "SNTransformerAddGradSeq",
+        ],
+    ),
+]
+trainer.add_modelbases(models)
+trainer.get_leaderboard(cross_validation=10)
+log.exit()
