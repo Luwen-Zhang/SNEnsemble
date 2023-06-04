844df92da31cef6ccd406a170a83aace3276c25e
LuoXueling <sharrin@sjtu.edu.cn>
2023-06-04 17:10:51 +0800

    sklearn type of GaussianMixture.

--- src/model/_transformer/clustering/common/gmm.py
+++ src/model/_transformer/clustering/common/gmm.py
@@ -7,40 +7,9 @@ from torch import nn
 import numpy as np
 from typing import List, Union
 import warnings
-from math import pi
 from .kmeans import KMeans
 from .base import AbstractCluster, AbstractClustering, AbstractMultilayerClustering
 from src.model._transformer.pca.incremental_pca import IncrementalPCA
-
-
-def calculate_matmul_n_times(n_components, mat_a, mat_b):
-    """
-    Calculate matrix product of two matrics with mat_a[0] >= mat_b[0].
-    Bypasses torch.matmul to reduce memory footprint.
-    args:
-        mat_a:      torch.Tensor (n, k, 1, d)
-        mat_b:      torch.Tensor (1, k, d, d)
-    """
-    res = torch.zeros(mat_a.shape).to(mat_a.device)
-
-    for i in range(n_components):
-        mat_a_i = mat_a[:, i, :, :].squeeze(-2)
-        mat_b_i = mat_b[0, i, :, :]
-        res[:, i, :, :] = mat_a_i.mm(mat_b_i).unsqueeze(1)
-
-    return res
-
-
-def calculate_matmul(mat_a, mat_b):
-    """
-    Calculate matrix product of two matrics with mat_a[0] >= mat_b[0].
-    Bypasses torch.matmul to reduce memory footprint.
-    args:
-        mat_a:      torch.Tensor (n, k, 1, d)
-        mat_b:      torch.Tensor (n, k, d, 1)
-    """
-    assert mat_a.shape[-2] == 1 and mat_b.shape[-1] == 1
-    return torch.sum(mat_a.squeeze(-2) * mat_b.squeeze(-1), dim=2, keepdim=True)
 
 
 class Cluster(AbstractCluster):
@@ -48,7 +17,10 @@ class Cluster(AbstractCluster):
         super(Cluster, self).__init__(n_input=n_input, momentum=momentum, **kwargs)
         self.register_buffer("mu", torch.randn(1, n_input))
         self.register_buffer("var", torch.randn(1, n_input, n_input))
-        self.register_buffer("pi", torch.ones(1, 1))
+        self.register_buffer(
+            "pi",
+            torch.ones(1),
+        )
 
     def update(self, mu=None, var=None, pi=None, momentum=None):
         momentum = self.momentum if momentum is None else momentum
@@ -94,107 +66,91 @@ class GMM(AbstractClustering):
         )
 
     def forward(self, x: torch.Tensor):
-        x = x.float()
         if not self.initialized and self.training:
             self.initialize(x)
-        x = self.modify_size(x)
+        # Ref: sklearn.mixture.BaseMixture.fit_predict
+        # Ref: sklearn.mixture.BaseMixture._estimate_weighted_log_porb
         # Calculate the probability that the ith sample belongs to the kth gaussian.
         # Note that the denominator in the bayesian theorem `log_prob_norm` (a constant) is not included.
-        weighted_log_prob = self._estimate_log_prob(x) + torch.log(self.pi)
+        weighted_log_prob = (
+            self._estimate_log_prob(x, self.mu, self.var) + self._estimate_log_weights()
+        )
         x_cluster = torch.argmax(weighted_log_prob, dim=1).squeeze(-1)
         if self.training:
             with torch.no_grad():
                 # E step
+                # Ref: sklearn.mixture.BaseMixture._estimate_log_prob_resp
                 # Calculate the denominator in the bayesian theorem
                 log_prob_norm = torch.logsumexp(weighted_log_prob, dim=1, keepdim=True)
                 # The log value of the probability that the ith sample belongs to the kth gaussian.
                 log_resp = weighted_log_prob - log_prob_norm
-
                 # M step
-                resp = torch.exp(log_resp)
-
-                pi = torch.sum(resp, dim=0, keepdim=True) + self.eps
-                mu = torch.sum(resp * x, dim=0, keepdim=True) / pi
-
-                eps = torch.eye(self.n_input, device=x.device) * self.eps
-                var = (
-                    torch.sum(
-                        (x - mu).unsqueeze(-1).matmul((x - mu).unsqueeze(-2))
-                        * resp.unsqueeze(-1),
-                        dim=0,
-                        keepdim=True,
-                    )
-                    / torch.sum(resp, dim=0, keepdim=True).unsqueeze(-1)
-                    + eps
+                # Ref: sklearn.mixture.GaussianMixture._m_step
+                weights, means, covariances = self._estimate_parameters(
+                    x, torch.exp(log_resp)
                 )
-                pi = pi / x.shape[0]
-
-                matched_clusters, counts = x_cluster.unique(return_counts=True)
-                lr = 1 / self.accum_n_points_in_clusters[:, None] * 0.9 + 0.1
-                self.accum_n_points_in_clusters[matched_clusters] += counts
-                for i_cluster, cluster in enumerate(self.clusters):
-                    cluster.update(
-                        mu=mu[:, i_cluster, :].view(1, -1),
-                        var=var[:, i_cluster, :, :],
-                        pi=pi[:, i_cluster, :],
-                        momentum=lr[i_cluster],
-                    )
+                self.update_params(
+                    weights=weights,
+                    means=means,
+                    covariances=covariances,
+                    labels=x_cluster,
+                )
         return x_cluster
 
     def initialize(self, x: torch.Tensor):
         if x.shape[0] < self.n_clusters:
             return None
-        pi = (
-            torch.ones((1, 1, self.n_clusters), requires_grad=False, device=x.device)
-            / self.n_clusters
-        )
-        var = (
-            torch.randn(
-                (self.n_input, self.n_input), requires_grad=False, device=x.device
-            )
-            .reshape(1, 1, self.n_input, self.n_input)
-            .repeat(1, self.n_clusters, 1, 1)
-        )
         if self.init_method == "random":
-            centers = x[torch.randperm(x.shape[0])[: self.n_clusters]]
+            resp = torch.rand(x.shape[0], self.n_clusters, device=x.device)
+            resp /= resp.sum(dim=1).unsqueeze(-1)
         elif self.init_method == "kmeans":
             kmeans = KMeans(n_clusters=self.n_clusters, n_input=self.n_input).to(
                 x.device
             )
             kmeans.fit(x, n_iter=10)
-            centers = kmeans.centers
             # Initial parameters are estimated using kmeans results.
-            # Reference: sklearn.mixture.gaussian_mixture._estimate_gaussian_covariances_full
-            # Estimate weights.
             labels = kmeans.predict(x)
-            cluster_found, found_counts = labels.unique(return_counts=True)
-            counts = torch.add(
-                torch.zeros((self.n_clusters,), device=x.device, dtype=torch.float32),
-                1e-12,
-            )
-            counts[cluster_found] = found_counts.float()
-            pi[0, 0, :] = counts / x.shape[0]
-            # Estimate variance and means.
-            for k in range(self.n_clusters):
-                resp = torch.zeros((x.shape[0],), device=x.device)
-                resp[torch.where(labels == k)[0]] = 1
-                centers[k, :] = torch.matmul(resp, x) / counts[k]
-                diff = x - centers[k, :]
-                var[0, k, :, :] = torch.matmul(resp * diff.t(), diff) / counts[
-                    k
-                ] + self.eps * torch.eye(self.n_input, device=x.device)
+            resp = torch.zeros((x.shape[0], self.n_clusters), device=x.device)
+            resp[torch.arange(x.shape[0]), labels] = 1
         else:
             raise Exception(
                 f"Initialization method {self.init_method} is not implemented."
             )
+        self._base_resp = resp
+        # Ref: sklearn.mixture.GaussianMixture._initialize
+        # Initialization is a special case of maximization step since we assume that labels are already known and
+        # the probabilities are ones.
+        weights, means, covariances = self._estimate_parameters(x, resp)
+        self.set_params(weights=weights, means=means, covariances=covariances)
+        self.initialized = True
 
+    def set_params(
+        self, weights: torch.Tensor, means: torch.Tensor, covariances: torch.Tensor
+    ):
         for i_cluster, cluster in enumerate(self.clusters):
             cluster.set(
-                mu=centers[i_cluster, :].view(1, -1),
-                var=var[:, i_cluster, :, :],
-                pi=pi[:, :, i_cluster],
+                mu=means[i_cluster, :].unsqueeze(0),
+                var=covariances[i_cluster, :, :].unsqueeze(0),
+                pi=weights[i_cluster],
             )
-        self.initialized = True
+
+    def update_params(
+        self,
+        weights: torch.Tensor,
+        means: torch.Tensor,
+        covariances: torch.Tensor,
+        labels,
+    ):
+        matched_clusters, counts = labels.unique(return_counts=True)
+        lr = 1 / self.accum_n_points_in_clusters[:, None] * 0.9 + 0.1
+        self.accum_n_points_in_clusters[matched_clusters] += counts
+        for i_cluster, cluster in enumerate(self.clusters):
+            cluster.update(
+                mu=means[i_cluster, :].unsqueeze(0),
+                var=covariances[i_cluster, :, :].unsqueeze(0),
+                pi=weights[i_cluster],
+                momentum=1,
+            )
 
     @property
     def centers(self):
@@ -202,86 +158,86 @@ class GMM(AbstractClustering):
 
     @property
     def mu(self):
-        return self.centers.unsqueeze(0)
+        return self.centers
 
     @property
     def var(self):
-        return torch.concat([cluster.var for cluster in self.clusters], 0).unsqueeze(0)
+        return torch.concat([cluster.var for cluster in self.clusters], 0)
 
     @property
     def pi(self):
-        pi = torch.concat([cluster.pi for cluster in self.clusters], 0).unsqueeze(0)
+        pi = torch.concat(
+            [cluster.pi.unsqueeze(-1) for cluster in self.clusters], 0
+        ).squeeze(-1)
         return pi / torch.sum(pi)
 
-    def modify_size(self, x):
-        if len(x.size()) == 2:
-            # (n, d) --> (n, 1, d)
-            x = x.unsqueeze(1)
-
-        return x
-
-    def _estimate_log_prob(self, x):
-        """
-        Returns a tensor with dimensions (n, k, 1), which indicates the log-likelihood that samples belong to the k-th Gaussian.
-        args:
-            x:            torch.Tensor (n, d) or (n, 1, d)
-        returns:
-            log_prob:     torch.Tensor (n, k, 1)
-        """
-        mu = self.mu
-        var = self.var
-        x = self.modify_size(x)
-        precision = torch.inverse(var.to(torch.float64)).to(x.dtype)
-        d = x.shape[-1]
-
-        log_2pi = d * np.log(2.0 * pi)
-
-        log_det = self._calculate_log_det(precision)
-
-        x_mu_T = (x - mu).unsqueeze(-2)
-        x_mu = (x - mu).unsqueeze(-1)
-
-        x_mu_T_precision = calculate_matmul_n_times(self.n_clusters, x_mu_T, precision)
-        x_mu_T_precision_x_mu = calculate_matmul(x_mu_T_precision, x_mu)
-
-        # `log_det` is the determinant of the inverse of the covariance, so the sign `+` is changed to `-`
-        return -0.5 * (log_2pi - log_det + x_mu_T_precision_x_mu)
-
-    def _calculate_log_det(self, var):
-        """
-        Calculate log determinant in log space, to prevent overflow errors.
-        args:
-            var:            torch.Tensor (1, k, d, d)
-        """
-        log_det = torch.empty(size=(self.n_clusters,)).to(var.device)
-
-        for k in range(self.n_clusters):
-            log_det[k] = (
-                2 * torch.log(torch.diagonal(torch.linalg.cholesky(var[0, k]))).sum()
+    def _compute_precision_cholesky(self, covariances: torch.Tensor):
+        # Ref: sklearn.mixture._gaussian_mixture._compute_precision_cholesky
+        n_components, n_features = self.n_clusters, self.n_input
+        precisions_chol = torch.zeros_like(covariances, device=covariances.device)
+        for k in range(n_components):
+            cov_chol = torch.linalg.cholesky(covariances[k, :, :].to(torch.float64)).to(
+                torch.float32
             )
-        # nan_to_num ignores invalid precision (inverse of variance)
-        log_det = torch.nan_to_num_(log_det, 0)
-        return log_det.unsqueeze(-1)
+            precisions_chol[k, :, :] = torch.linalg.solve_triangular(
+                cov_chol, torch.eye(n_features, device=cov_chol.device), upper=False
+            ).T
+        return precisions_chol
 
-    def __score(self, x, as_average=True):
-        """
-        Computes the log-likelihood of the data under the model.
-        args:
-            x:                  torch.Tensor (n, 1, d)
-            sum_data:           bool
-        returns:
-            score:              torch.Tensor (1)
-            (or)
-            per_sample_score:   torch.Tensor (n)
+    def _estimate_parameters(self, x: torch.Tensor, resp: torch.Tensor):
+        weights, means, covariances = self._estimate_gaussian_parameters(x, resp)
+        weights /= x.shape[0]
+        return weights, means, covariances
 
-        """
-        weighted_log_prob = self._estimate_log_prob(x) + torch.log(self.pi)
-        per_sample_score = torch.logsumexp(weighted_log_prob, dim=1)
+    def _estimate_gaussian_parameters(self, x: torch.Tensor, resp: torch.Tensor):
+        # Ref: sklearn.mixture._gaussian_mixture._estimate_gaussian_parameters
+        nk = torch.add(torch.sum(resp, dim=0), 1e-12)
+        means = torch.matmul(resp.T, x) / nk.unsqueeze(-1)
+        # Ref: sklearn.mixture._gaussian_mixture._estimate_gaussian_covariances_full
+        n_components, n_features = self.n_clusters, self.n_input
+        covariances = torch.zeros(
+            (n_components, n_features, n_features), device=x.device
+        )
+        for k in range(self.n_clusters):
+            diff = x - means[k, :]
+            covariances[k] = torch.matmul(resp[:, k] * diff.T, diff) / nk[
+                k
+            ] + self.eps * torch.eye(self.n_input, device=x.device)
+        return nk, means, covariances
 
-        if as_average:
-            return per_sample_score.mean()
-        else:
-            return torch.squeeze(per_sample_score)
+    def _estimate_log_prob(
+        self, x: torch.Tensor, means: torch.Tensor, covariances: torch.Tensor
+    ):
+        return self._estimate_log_gaussian_prob(x, means, covariances)
+
+    def _estimate_log_weights(self):
+        return torch.log(self.pi)
+
+    def _estimate_log_gaussian_prob(
+        self, x: torch.Tensor, means: torch.Tensor, covariances: torch.Tensor
+    ):
+        # Ref: sklearn.mixture._gaussian_mixture._initialize
+        precision_chol = self._compute_precision_cholesky(covariances)
+        # Ref: sklearn.mixture._gaussian_mixture._estimate_log_gaussian_prob
+        log_det = self._compute_log_det_cholesky(precision_chol)
+
+        log_prob = torch.zeros(x.shape[0], self.n_clusters, device=x.device)
+        for k in range(self.n_clusters):
+            mu = means[k, :]
+            prec_chol = precision_chol[k, :, :]
+            y = torch.matmul(x, prec_chol) - torch.matmul(mu, prec_chol)
+            log_prob[:, k] = torch.sum(torch.square(y), dim=1)
+        return -0.5 * (self.n_input * np.log(2 * np.pi) + log_prob) + log_det
+
+    def _compute_log_det_cholesky(self, precision_chol: torch.Tensor):
+        # Ref: sklearn.mixture._gaussian_mixture._compute_log_det_cholesky
+        log_det_chol = torch.sum(
+            torch.log(
+                precision_chol.reshape(self.n_clusters, -1)[:, :: self.n_input + 1]
+            ),
+            dim=1,
+        )
+        return log_det_chol
 
 
 class PCAGMM(GMM):
