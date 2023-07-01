84c2234468ad5f92e711e55a70b03e069b3a8682
LuoXueling <sharrin@sjtu.edu.cn>
2023-07-01 11:00:44 +0800

    Move the correlation chain calculation to a util function

--- src/data/dataprocessor.py
+++ src/data/dataprocessor.py
@@ -14,6 +14,7 @@ from sklearn.feature_selection import VarianceThreshold
 from sklearn.preprocessing import StandardScaler as skStandardScaler
 from sklearn.preprocessing import OrdinalEncoder
 from typing import Type
+from .utils import get_corr_sets
 
 
 class SampleDataAugmentor(AbstractAugmenter):
@@ -354,44 +355,9 @@ class CorrFeatureSelector(AbstractFeatureSelector):
 
         abs_corr = datamodule.cal_corr(imputed=False, features_only=True).abs()
         where_corr = np.where(abs_corr > thres)
-        where_corr = [[datamodule.cont_feature_names[x] for x in y] for y in where_corr]
-        corr_chain = {}
-
-        def add_edge(x, y):
-            if x not in corr_chain.keys():
-                corr_chain[x] = [y]
-            elif y not in corr_chain[x]:
-                corr_chain[x].append(y)
-
-        for x, y in zip(*where_corr):
-            if x != y:
-                add_edge(x, y)
-                add_edge(y, x)
-        corr_feature = list(corr_chain.keys())
-        for x in np.setdiff1d(datamodule.cont_feature_names, corr_feature):
-            corr_chain[x] = []
-
-        def dfs(visited, graph, node, ls):
-            if node not in visited:
-                ls.append(node)
-                visited.add(node)
-                for neighbour in graph[node]:
-                    ls = dfs(visited, graph, neighbour, ls)
-            return ls
-
-        corr_sets = []
-        for x in corr_feature[::-1]:
-            if len(corr_sets) != 0:
-                for sets in corr_sets:
-                    if x in sets:
-                        break
-                else:
-                    corr_sets.append(dfs(set(), corr_chain, x, []))
-            else:
-                corr_sets.append(dfs(set(), corr_chain, x, []))
-
-        corr_sets = [[x for x in y] for y in corr_sets]
-
+        corr_feature, corr_sets = get_corr_sets(
+            where_corr, datamodule.cont_feature_names
+        )
         rf = datamodule.get_base_predictor(
             categorical=False, n_estimators=n_estimators, n_jobs=-1, random_state=0
         )
