7f61229fa2a3e2b852d7ffa708583e4fe5fa6dd6
LuoXueling <sharrin@sjtu.edu.cn>
2024-03-22 15:01:11 +0800

    Update execution scripts.

--- main.py
+++ main.py
@@ -5,21 +5,8 @@ import tabensemb
 from tabensemb.model import *
 from tabensemb.utils import Logging
 import os
-import argparse
 
 tabensemb._stream_filters = ["DeprecationWarning"]
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--reduce_bayes_steps", dest="reduce_bayes_steps", action="store_true"
-)
-parser.add_argument(
-    "--investigate_clustering", dest="investigate_clustering", action="store_true"
-)
-parser.set_defaults(**{"reduce_bayes_steps": False, "investigate_clustering": False})
-args = parser.parse_known_args()[0]
-reduce_bayes_steps = args.reduce_bayes_steps
-investigate_clustering = args.investigate_clustering
 
 log = Logging()
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -34,34 +21,8 @@ models = [
     PytorchTabular(trainer),
     WideDeep(trainer),
     AutoGluon(trainer),
-    ThisWork(
-        trainer, reduce_bayes_steps=reduce_bayes_steps, pca=False, clustering="KMeans"
-    ),
+    ThisWork(trainer, pca=False, clustering="KMeans", clustering_layer="2L"),
 ]
-if investigate_clustering:
-    models += [
-        ThisWork(
-            trainer,
-            reduce_bayes_steps=reduce_bayes_steps,
-            pca=True,
-            clustering="KMeans",
-            program="ThisWork_PCA",
-        ),
-        ThisWork(
-            trainer,
-            reduce_bayes_steps=reduce_bayes_steps,
-            pca=False,
-            clustering="GMM",
-            program="ThisWork_GMM",
-        ),
-        ThisWork(
-            trainer,
-            reduce_bayes_steps=reduce_bayes_steps,
-            pca=False,
-            clustering="BMM",
-            program="ThisWork_BMM",
-        ),
-    ]
 trainer.add_modelbases(models)
 trainer.get_leaderboard(cross_validation=10, split_type="random")
 log.exit()
--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@
 #python main.py --reduce_bayes_steps --base additively_manufactured_alloys --bayes_opt --data_splitter MaterialSplitter --split_ratio 0.3 0.1 0.6
 #python main.py --reduce_bayes_steps --base additively_manufactured_alloys --bayes_opt --data_splitter MaterialSplitter
 
-python main.py --base composite --bayes_opt --data_splitter StressCycleSplitter --split_ratio 0.3 0.1 0.6
 python main.py --base composite --bayes_opt --data_splitter StressCycleSplitter
+python main.py --base composite --bayes_opt --data_splitter StressCycleSplitter --split_ratio 0.3 0.1 0.6
 
-python main.py --base composite --bayes_opt --data_splitter RandomSplitter --split_ratio 0.3 0.1 0.6
 python main.py --base composite --bayes_opt --data_splitter RandomSplitter
+python main.py --base composite --bayes_opt --data_splitter RandomSplitter --split_ratio 0.3 0.1 0.6
 
 python main_clustering.py --base composite --bayes_opt --data_splitter RandomSplitter
 python main_clustering.py --base composite --bayes_opt --data_splitter StressCycleSplitter
