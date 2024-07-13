84385a9b4dbea880896bae9b9cf17546bfed6106
LuoXueling <sharrin@sjtu.edu.cn>
2024-07-14 00:11:06 +0800

    Change shap models to this work.

--- calculate_shap.py
+++ calculate_shap.py
@@ -16,16 +16,19 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 trainer = load_trainer("output/analyse/paper_plots/trainer.pkl")
 
 trainer_analysis = load_trainer(
-    "output/composite_database_03222024/2024-04-16-23-10-09-0_composite A622 mcd analysis/trainer.pkl"
+    "output/composite_database_03222024/2024-04-16-23-10-09-0_composite A811 mcd analysis/trainer.pkl"
 )
 
 shap_importance_full_train_ls = []
 explainers = []
 explanations = []
-for program, model_name in zip(
-    trainer_analysis.leaderboard["Program"][:10],
-    trainer_analysis.leaderboard["Model"][:10],
-):
+
+leaderboard = trainer_analysis.leaderboard.sort_values(by="Testing MAPE")
+program = "ThisWork"
+model_names = leaderboard.loc[leaderboard["Program"] == program, "Model"][:10]
+print(model_names)
+
+for model_name in model_names:
     (explainer, explanation, shap_value), feature_names = (
         trainer_analysis.cal_feature_importance(
             program=program,
@@ -42,7 +45,7 @@ for program, model_name in zip(
     explainers.append(explainer)
     explanations.append(explanation)
 
-with open(os.path.join(trainer.project_root, "shap_train.pkl"), "wb") as file:
+with open(os.path.join(trainer.project_root, "shap_train_ensemble.pkl"), "wb") as file:
     pickle.dump(
         (explainers, explanations, shap_importance_full_train_ls, feature_names), file
     )
